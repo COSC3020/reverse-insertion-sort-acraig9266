31590f10f7408b0011ba3e8c4a1480d0651f1835
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-09-05 23:39:28 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 function insertionSortReverse(array) {
-    return array;
+    for (var i = array.length; i > -1; i--) {
+        var val = array[i-1]
+        var j;
+        for (j = i; j > 0 && array[j + 1] < val; j++) {
+            array[j] = array[j + 1];
+        }
+        array[j] = val
+    }
+    return array.slice(1, array.length);
 }
